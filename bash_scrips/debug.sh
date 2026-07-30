8254f9cbf7c3bf6b42195d8acc877acd50b54c00
yunjie-sysu <127676386+yunjie-sysu@users.noreply.github.com>
2026-07-30 11:51:39 +0800

    Update debug.sh

--- bash_scrips/debug.sh
+++ bash_scrips/debug.sh
@@ -12,8 +12,8 @@ ACCELERATE_LOG_LEVEL=info $ACCELERATE launch \
     --mixed_precision bf16 \
     --num_processes 1 \
     regen_train.py configs/config_regen.yaml \
-    --model_name_or_path="/data/vjuicefs_ai_gpt_wl/public_data/11195618/yunjie/base_model/Qwen2.5-1.5B-Instruct" \
-    --data_path="/data/vjuicefs_ai_gpt_wl/public_data/11195618/yunjie/offline-RL/REGEN/data/train/Trajectories" \
+    --model_name_or_path="Qwen2.5-1.5B-Instruct" \
+    --data_path="data/train/Trajectories" \
     --per_device_train_batch_size=1 \
     --gradient_accumulation_steps=1 \
     --torch_dtype=bfloat16 \
