a3969f42a42be47fe74dd99228cc8f551b6e461e
yunjie-sysu <127676386+yunjie-sysu@users.noreply.github.com>
2026-07-30 11:51:04 +0800

    Update qwen2_5_1.5b_regen.sh

--- bash_scrips/qwen2_5_1.5b_regen.sh
+++ bash_scrips/qwen2_5_1.5b_regen.sh
@@ -6,8 +6,8 @@ ACCELERATE_LOG_LEVEL=info accelerate launch \
     --mixed_precision=bf16 \
     --num_processes 4 \
     regen_train.py configs/config_regen.yaml \
-    --model_name_or_path=/data/vjuicefs_ai_gpt_wl/public_data/11195618/yunjie/base_model/Qwen2.5-1.5B-Instruct\
-    --data_path="data/train/Teacher-Online-All-V5" \
+    --model_name_or_path=Qwen2.5-1.5B-Instruct\
+    --data_path="data/train/Trajectories" \
     --per_device_train_batch_size=4 \
     --gradient_accumulation_steps=8 \
     --learning_rate=5e-7 \
